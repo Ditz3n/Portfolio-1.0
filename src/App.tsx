561e95dde97721af0e22b69eab569a12b1df8d69
Mads Dittmann Villadsen <202209869@post.au.dk>
2024-12-16 19:18:18 +0100

    Test til at fixe modalvinduet gør, at det ikke virker på mobil.

--- src/App.tsx
+++ src/App.tsx
@@ -44,7 +44,7 @@ function App() {
     if (isModalOpen) {
       document.body.style.overflow = 'hidden';
     } else {
-      document.body.style.overflow = 'unset';
+      document.body.style.overflow = 'auto';
     }
   }, [isModalOpen]);
 
